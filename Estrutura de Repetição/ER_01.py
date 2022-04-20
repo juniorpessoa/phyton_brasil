341be552fc0d5ce79fb43f985208049be3016afd
juniorpessoa <80985875+juniorpessoa@users.noreply.github.com>
2022-04-19 23:57:18 -0300

    Create ER_01.py

---
+++ Estrutura de Repetição/ER_01.py
@@ -0,0 +1,13 @@
+"""
+Ex01:
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo até que o usuário informe um valor válido.
+"""
+n = int(input('Entre com uma nota: '))
+
+if 0 <= n <= 10:
+  print(n)
+else:
+    for 0>= n >= 10 in n:
+        print('teste')
