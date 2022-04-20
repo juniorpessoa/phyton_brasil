30c85b5bd7ac3d07b2c1870de3242fdcf2c02ec9
juniorpessoa <80985875+juniorpessoa@users.noreply.github.com>
2022-04-20 07:38:34 -0300

    Create ed02.py

---
+++ EstruturaDeDecisao/ed02.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+"""
+numero = int(input('Entre com um número:'))
+
+if numero >= 0:
+    print('Número é positivo')
+else:
+    print('Numero negativo')
