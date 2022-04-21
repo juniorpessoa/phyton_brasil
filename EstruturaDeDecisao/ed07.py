3c926b46cbc2e9fdbf5e025ea5614c9eb3b06d85
juniorpessoa <80985875+juniorpessoa@users.noreply.github.com>
2022-04-20 22:28:08 -0300

    Create ed07.py

---
+++ EstruturaDeDecisao/ed07.py
@@ -0,0 +1,23 @@
+"""
+Faça um Programa que leia
+três números e mostre o maior deles.
+"""
+
+n1 = int(input('Entre com o 1° número: '))
+n2 = int(input('Entre com o 2° número: '))
+n3 = int(input('Entre com o 3° número: '))
+
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n1:
+    menor = n3
+print('-=-'*12)
+print('O maior número é {}'.format(maior))
+print('O menor número é {}'.format(menor))
