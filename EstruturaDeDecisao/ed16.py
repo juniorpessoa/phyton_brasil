0ee65e5a9a85f0843bfd3b9064a81da6ff87477d
juniorpessoa <ssdesigner.online@gmail.com>
2022-04-25 15:46:19 -0300

    Create ed16.py

---
+++ EstruturaDeDecisao/ed16.py
@@ -0,0 +1,37 @@
+"""
+Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c. 
+O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes situações:
+
+[+]Se o usuário informar o valor de A igual a zero, a equação não é do segundo grau e o programa não deve fazer pedir os demais valores, 
+sendo encerrado;
+[+]Se o delta calculado for negativo, a equação não possui raizes reais. Informe ao usuário e encerre o programa;
+[+]Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe-a ao usuário;
+[+]Se o delta for positivo, a equação possui duas raiz reais; informe-as ao usuário;
+"""
+
+print('Entre com os dados:')
+
+
+n1 = int(input('VALOR DE A: '))
+if n1 != 0:
+    n2 = int(input('VALOR DE B: '))
+    n3 = int(input('VALOR DE C: '))
+    n4 = int(input('VALOR DE D: '))
+    verificar = -1
+    if 0 > verificar:
+        print('-=-'*16)
+        print('''[+]Se o delta calculado for negativo, a equação 
+não possui raizes reais.Informe ao usuário 
+e encerre o programa;''')
+        print('-=-'*16)
+    elif n4 == 0:
+        print(''' [+]Se o delta calculado for igual a zero a equação possui apenas uma raiz real;
+        informe-a ao usuário
+        ''')
+else:
+    print('-=-'*18)
+    print('''[+]Se o usuário informar o valor de A igual a zero, 
+a equação não é do segundo grau e o programa não deve 
+fazer pedir os demais valores,sendo encerrado;''')
+    print('-=-'*18)
+ 
