6b5d33f30ef54d5827b05aa13a887450fe07a8dd
juniorpessoa <80985875+juniorpessoa@users.noreply.github.com>
2022-04-27 22:59:56 -0300

    Update ER_01.py

--- Estrutura de Repetição/ER_01.py
+++ Estrutura de Repetição/ER_01.py
@@ -1,7 +1,9 @@
 """
-Ex01:
 Faça um programa que peça uma nota, entre zero e dez.
-Mostre uma mensagem caso o valor seja inválido
-e continue pedindo até que o usuário informe um valor válido.
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 """
-print('Teste')
+
+n= 0
+while 0 >= n <=10:
+    n = int(input('Entre com número válido: '))
+print('fim')
