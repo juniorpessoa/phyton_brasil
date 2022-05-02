50d1802e26d0cadbf98dab674fd9210e227b0de3
juniorpessoa <80985875+juniorpessoa@users.noreply.github.com>
2022-05-02 18:55:07 -0300

    Create ER06.py

---
+++ Estrutura de Repetição/ER06.py
@@ -0,0 +1,25 @@
+"""
+Faça um programa que imprima na tela os números de 1 a 20,
+um abaixo do outro. Depois modifique o programa para que ele
+mostre os números um ao lado do outro.
+
+cont = 0
+cont2 = 0
+
+while 10 >= cont:
+    print(cont)
+    cont += 1
+
+print('')
+while 10 >= cont2:
+    print(cont2, end= ' ')
+    cont2 += 1
+print('')
+
+print('fim')
+"""
+
+#resposta verificad na internet pois  utilizamos o for quando
+for i in range (21):
+	print (i)
+print(list(range(1,21)))
