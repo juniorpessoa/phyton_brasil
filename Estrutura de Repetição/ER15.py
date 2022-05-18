09445c478169b48d796153e1687d90618a1fb82e
juniorpessoa <ssdesigner.online@gmail.com>
2022-05-18 14:36:55 -0300

    Create ER15.py

---
+++ Estrutura de Repetição/ER15.py
@@ -0,0 +1,25 @@
+"""
+A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... 
+Faça um programa capaz de gerar a série até o n−ésimo termo.
+
+cont = 0
+n = int(input('Entre com um número: '))
+
+for c in range(n):
+    print((c),(c*7),end='')
+"""
+
+
+n = int(input("Que termo deseja encontrar: "))
+ultimo=1
+penultimo=1
+
+if (n==1) or (n==2):
+    print("1")
+else:
+    for count in range(2,n):
+        termo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = termo
+        count += 1
+    print(termo)
