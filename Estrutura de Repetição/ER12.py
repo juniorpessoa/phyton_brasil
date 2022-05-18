7e1a06436deccf06b66d1727b36872d637339857
juniorpessoa <ssdesigner.online@gmail.com>
2022-05-18 07:52:21 -0300

    Update ER12.py

--- Estrutura de Repetição/ER12.py
+++ Estrutura de Repetição/ER12.py
@@ -2,5 +2,9 @@
 Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
 O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
 """
-
-tabuada = int (input('ENTRE COM UMA TABUDA: '))
+cont = 0
+n = int (input('ENTRE COM UMA TABUDA: '))
+print('')
+for c in range (0,11):
+    print(f'{n} x {cont} = {n*cont}')
+    cont += 1
