bf40689924212e6f22b81a285534f00efe43f56e
juniorpessoa <ssdesigner.online@gmail.com>
2022-05-18 07:21:11 -0300

    Update ER10.py

--- Estrutura de Repetição/ER10.py
+++ Estrutura de Repetição/ER10.py
@@ -3,11 +3,20 @@ Faça um programa que receba dois números inteiros
 e gere os números inteiros que estão no intervalo compreendido por eles.
 """
 
+from re import I
+
+
+soma = 0
 while True:
     n = int(input('Entre com um número: '))
     n2 = int(input('Entre com outro número: '))
-
+    print('-=-'*18)
     for c in range(n,n2):
         print((c),end= ' ')
+        soma += c
+    print('')
+    print('-=-'*18)
+    print(f'\nA soma dos número : {soma}')
+    break
 
         
