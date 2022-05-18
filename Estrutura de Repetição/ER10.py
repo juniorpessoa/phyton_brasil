2956a50573706461cb8b8aa96544e4753c054e4c
juniorpessoa <ssdesigner.online@gmail.com>
2022-05-18 06:37:16 -0300

    Update ER10.py

--- Estrutura de Repetição/ER10.py
+++ Estrutura de Repetição/ER10.py
@@ -3,7 +3,7 @@ Faça um programa que receba dois números inteiros
 e gere os números inteiros que estão no intervalo compreendido por eles.
 """
 
-for True:
+while True:
     n = int(input('Entre com um número: '))
     n2 = int(input('Entre com outro número: '))
 
