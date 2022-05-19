7aa11572bdb42512846105a403a4b6251b7dd257
juniorpessoa <ssdesigner.online@gmail.com>
2022-05-19 14:49:27 -0300

    Create ER18.py

---
+++ Estrutura de Repetição/ER18.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
+"""
+
+print('-='*20)
+menor = maior = 0
+cont = 0
+
+escolha = int(input('Entre com a quantidade de número desejado: '))
+print(escolha)
+for c in range(1,escolha+1):
+    n = int(input(f'Entre com o {c} número: '))
