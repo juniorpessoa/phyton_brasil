e7213528b09875a0a0d829c02a13facfd58022cb
juniorpessoa <ssdesigner.online@gmail.com>
2022-05-19 08:57:33 -0300

    Create ER17.py

---
+++ Estrutura de Repetição/ER17.py
@@ -0,0 +1,14 @@
+"""
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+ Ex.: 5!=5.4.3.2.1=120
+"""
+
+from math import factorial
+
+while True:
+    n = int(input('Entre com valor FATORIAL: '))
+    print ("Factorial is", factorial(5))
+
+
+
+
